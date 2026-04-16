0a23e617759987fdbe87cbe17d04cab0dc6707ce
potato987. <55237095+that1potato@users.noreply.github.com>
2026-04-16 14:39:55 +1000

    Update page title

--- control-frontend/src/pages/Dashboard/Home.tsx
+++ control-frontend/src/pages/Dashboard/Home.tsx
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <>
       <PageMeta
-        title="CampusRide - Real-time Control Dashboard for Robot Management"
-        description="Real-time control dashboard for robot management by Herman Tang"
+        title="Control Dashboard"
+        description="DishPatch | Real-time control dashboard for robot management."
       />
       <div className="grid grid-cols-12 gap-4 md:gap-6">
         {/* <div className="col-span-12 space-y-6 xl:col-span-7">
