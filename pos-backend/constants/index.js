0bc2d21e5b5e1a05b4090d22890d6f0b8e3b915d
HermanT <u7436114@anu.edu.au>
2026-05-09 01:24:52 +1000

    Update dish names in constants/index.js and upload photo

--- pos-backend/constants/index.js
+++ pos-backend/constants/index.js
@@ -20,13 +20,13 @@ export const pasta = [
   {
     id: 4,
     uuid: "44aced78-4b7c-4ccc-b4e6-f54daade44f6",
-    name: "Bolognese Pasta",
+    name: "Homemade herb sausage",
     price: 19,
   },
   {
     id: 5,
     uuid: "f3fa534a-0c99-4007-ac68-3d6618ee3279",
-    name: "Chicken and Mushroom Pasta",
+    name: "Pescatore",
     price: 19.5,
   },
 ];
@@ -53,65 +53,26 @@ export const desserts = [
   {
     id: 4,
     uuid: "33099972-716e-4bc0-a00c-34ce928cb7ba",
-    name: "Custard MarronCake",
+    name: "Apple Mango Cake",
     price: 9.3,
   },
   {
     id: 5,
     uuid: "49cf4291-0409-49d5-b107-caf92b39ca49",
-    name: "Marron ChocolateCake",
+    name: "Fresh Fruit Cake",
     price: 10.8,
   },
   {
     id: 6,
     uuid: "b661ba3b-cdab-4d45-a36a-3323e17f91db",
-    name: "Marron Glaces Cake",
+    name: "Orange Mousse Tart",
     price: 10.8,
   },
   {
     id: 7,
     uuid: "b8fdc784-b93d-4b9f-bffb-1227673b701a",
-    name: "Fresh Fruits Cake",
+    name: "Chocolate Cake",
     price: 11.5,
-  },
-];
-
-export const coffee = [
-  {
-    id: 1,
-    uuid: "f7a131ee-6f54-4901-a490-5472b0c79d34",
-    name: "Flat white (M)",
-    price: 5,
-  },
-  {
-    id: 2,
-    uuid: "5cce45ce-1ebf-49c5-ac69-92a7160c28b7",
-    name: "Flat white (L)",
-    price: 5,
-  },
-  {
-    id: 3,
-    uuid: "0478f591-72ff-4da7-9e28-63200dd82aeb",
-    name: "Latte (M)",
-    price: 5,
-  },
-  {
-    id: 4,
-    uuid: "a2a53ac2-0fb9-4340-bf19-f6dfb7388aac",
-    name: "Latte (L)",
-    price: 5,
-  },
-  {
-    id: 5,
-    uuid: "f3b1ec98-8ed4-498a-aa8a-5b15e2013879",
-    name: "Cappuccino (M)",
-    price: 5,
-  },
-  {
-    id: 6,
-    uuid: "61e235e3-a365-4f85-b596-76e8247cea81",
-    name: "Cappuccino (L)",
-    price: 5,
   },
 ];
 
@@ -139,14 +100,6 @@ export const sandwiches = [
 export const menus = [
   {
     id: 1,
-    uuid: "9f139f55-4600-4beb-8619-a60cd788d804",
-    name: "Coffee",
-    bgColor: "#b73e3e",
-    icon: "☕",
-    items: coffee,
-  },
-  {
-    id: 2,
     uuid: "7adbfa22-96e6-4643-b262-5063868d7eb1",
     name: "Seasonal Pasta",
     bgColor: "#5b45b0",
@@ -154,7 +107,7 @@ export const menus = [
     items: pasta,
   },
   {
-    id: 3,
+    id: 2,
     uuid: "d09e2023-2063-4751-9392-88fde302077c",
     name: "Desserts",
     bgColor: "#1d2569",
@@ -162,7 +115,7 @@ export const menus = [
     items: desserts,
   },
   {
-    id: 4,
+    id: 3,
     uuid: "96d78ae1-6b54-417c-b76c-e41364d22b93",
     name: "Sandwiches",
     bgColor: "#5b45b0",
